ab0f3fb6c6565dbc44841714c17e6ce1938d1b0a
nounen <1394139539@qq.com>
2018-01-03 00:21:17 +0800

    按钮之间添加空格

--- resources/views/admin/apply/deal_wait.blade.php
+++ resources/views/admin/apply/deal_wait.blade.php
@@ -52,6 +52,7 @@
 
                 <div class="box-footer text-center" style="border: none;">
                     <button type="submit" class="btn btn-info">&nbsp;&nbsp;确定&nbsp;&nbsp;</button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                     <button type="" class="btn">&nbsp;&nbsp;返回&nbsp;&nbsp;</button>
                 </div>
             </div>
