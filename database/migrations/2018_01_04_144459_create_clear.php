ca06f5631f09dd9ba36fdc180e0cc385b7a65626
nounen <1394139539@qq.com>
2018-01-04 22:53:58 +0800

    创建结算表

---
+++ database/migrations/2018_01_04_144459_create_clear.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateClear extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clear', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('start_date')->comment('结算开始日期: 包含');
+            $table->date('end_date')->comment('结算截止日期: 包含');
+            $table->decimal('income')->comment('收入');
+            $table->decimal('profit')->comment('利润');
+            $table->integer('clear_state')->default(0)->comment('结算状态: 0 未处理; 1 等待结算 2 已结算;');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clear');
+    }
+}
