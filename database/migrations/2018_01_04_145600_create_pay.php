8f8ea0a1c679def53c2b6b1801f90028cb703c4f
nounen <1394139539@qq.com>
2018-01-06 23:56:53 +0800

    pay 表字段补充

--- database/migrations/2018_01_04_145600_create_pay.php
+++ database/migrations/2018_01_04_145600_create_pay.php
@@ -15,11 +15,13 @@ class CreatePay extends Migration
     {
         Schema::create('pay', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('wechat_user_id')->comment('提现微信用户');
             $table->integer('pay_type')->comment('支付方式: 1 支付宝; 2 微信');
             $table->string('account')->comment('账号');
             $table->string('real_name')->comment('真实姓名');
             $table->decimal('price')->comment('提现金额');
             $table->integer('pay_state')->comment('状态: 1. 申请提现; 2. 已转');
+            $table->integer('admin_user_id')->default(0)->comment('最后一次操作本条记录的后台用户');
             $table->timestamps();
             $table->softDeletes();
         });
