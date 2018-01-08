a2f0c9a06621097f62fc490f397727711925d1ce
nounen <1394139539@qq.com>
2018-01-08 14:36:48 +0800

    二维码生成, 并当作 logo 使用的案例

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+use Intervention\Image\ImageManager;
 
 Route::get('/', function () {
 
@@ -18,4 +19,17 @@ Route::get('/', function () {
         \Auth::guard('wechat')->user()
     );
 //    return view('welcome');
+});
+
+Route::get('/qr_code', function () {
+    // 生成二维码
+    // QrCode::format('png')->size(80)->margin(0.2)->generate('Make me into a QrCode!', '../public/qrcode.png');
+
+    // 二维码当作 log 放在另一张图的某个位置
+    $im = new ImageManager();
+    // $im->configure(array('driver' => 'gd'));
+    $warter = $im->make("../public/qrcode.png");
+    $image = $im->make("../public/qrcode_background.png");
+    $image->insert($warter, 'top-left', 70, 55);
+    $image->save("../public/new_qrcode.png");
 });
