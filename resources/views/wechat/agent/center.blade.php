114caa1accd781a09091832fb7179f49c566e8ea
lazyou <1394139539@qq.com>
2018-01-12 23:21:09 +0800

    wechat: 代理中心图标

--- resources/views/wechat/agent/center.blade.php
+++ resources/views/wechat/agent/center.blade.php
@@ -3,29 +3,65 @@
 @section('content')
     @include('wechat.common.wechat_header')
 
+    <style>
+        .icon {
+            width: 40px;
+        }
+    </style>
+
     <div style="padding-top: 30px;"></div>
 
     <div class="row text-center">
         <a href="{{ url('wechat/agent/rule') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">代理规则</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/rule.png') }}">
+                </div>
+                代理规则
+            </div>
         </a>
         <a href="{{ url('wechat/agent/qr_code') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">推广二维码</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/qr_code.png') }}">
+                </div>
+                推广二维码
+            </div>
         </a>
         <a href="{{ url('wechat/agent/my_user') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">我的团队</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/my_user.png') }}">
+                </div>
+                我的团队
+            </div>
         </a>
     </div>
 
     <div class="row text-center">
         <a href="{{ url('wechat/agent/my_car') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">我的车库</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/my_car.png') }}">
+                </div>
+                我的车库
+            </div>
         </a>
         <a href="{{ url('wechat/agent/my_account') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">我的账户</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/my_account.png') }}">
+                </div>
+                我的账户
+            </div>
         </a>
         <a href="{{ url('wechat/agent/apply') }}" style="color: #333;">
-            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 50px;">申请提现</div>
+            <div class="col-xs-4" style="padding: 20px; border: 1px solid #ccc; line-height: 30px;">
+                <div>
+                    <img class="icon" src="{{ asset('images/apply.png') }}">
+                </div>
+                申请提现
+            </div>
         </a>
     </div>
 @endsection
